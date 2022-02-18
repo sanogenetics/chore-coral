7c811932fdf295deceb6700ba6681c5ce715cb83
Adam Faulconbridge <adam@sanogenetics.com>
2022-02-18 15:20:06 +0000

    implement _get_or_create_blueprint

--- chorecoral/__init__.py
+++ chorecoral/__init__.py
@@ -12,6 +12,14 @@ class JobQueueMismatchError(Exception):
     pass
 
 
+class JobBlueprintMismatchError(Exception):
+    pass
+
+
+class JobBlueprintCreationError(Exception):
+    pass
+
+
 class Builder:
     def __init__(self):
         pass
@@ -237,6 +245,166 @@ class Builder:
         else:
             return self._create_queue(batch_client, name, compute_environment_arn)
 
+    def _get_blueprint(
+        self,
+        batch_client,
+        name: str,
+        image: str,
+        vcpu: Union[float, int],
+        memory: int,
+        command: Iterable[str],
+    ) -> str:
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/batch.html#Batch.Client.describe_job_definitions
+
+        nextToken = None
+        first = True
+        while first or nextToken:
+            kwargs = {}
+            # handle a non-first page
+            if nextToken:
+                kwargs["nextToken"] = nextToken
+
+            response = batch_client.describe_job_definitions(
+                **kwargs,
+            )
+            for blueprint in response["jobDefinitions"]:
+                if blueprint["jobDefinitionName"] == name:
+                    # found a name match
+
+                    if blueprint["type"] != "container":
+                        raise JobBlueprintMismatchError(f"type is {blueprint['type']}")
+                    if blueprint["containerProperties"]["image"] != image:
+                        raise JobBlueprintMismatchError(
+                            f"image is {blueprint['containerProperties']['image']} not {image}"
+                        )
+                    if float(blueprint["containerProperties"]["vcpus"]) != vcpu:
+                        raise JobBlueprintMismatchError(
+                            f"vcpu is {blueprint['containerProperties']['vcpus']} not {vcpu}"
+                        )
+                    if int(blueprint["containerProperties"]["memory"]) != memory:
+                        raise JobBlueprintMismatchError(
+                            f"memory is {blueprint['containerProperties']['memory']} not {memory}"
+                        )
+                    if tuple(blueprint["containerProperties"]["command"]) != tuple(
+                        command
+                    ):
+                        raise JobBlueprintMismatchError(
+                            f"command is {blueprint['containerProperties']['command']} not {command}"
+                        )
+
+                    # TODO more validation
+
+                    # got to here without problem so we can use it
+                    return blueprint["jobDefinitionArn"]
+
+            # mark that we've finished the first page
+            first = False
+            # move to the next page, if applicable
+            if "nextToken" in response and response["nextToken"]:
+                nextToken = response["nextToken"]
+            else:
+                # no next page
+                nextToken = None
+
+        # no match found
+        return None
+
+    def _create_blueprint(
+        self,
+        batch_client,
+        name: str,
+        image: str,
+        vcpu: Union[float, int],
+        memory: int,
+        command: Iterable[str],
+    ) -> str:
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/batch.html#Batch.Client.register_job_definition
+
+        # only certain vcpu & memory combinations allowed on Fargate
+        permitted_memory_vcpu = {
+            512: (0.25,),
+            1024: (0.25, 0.5),
+            2048: (0.25, 0.5, 1),
+            3072: (0.5, 1),
+            4096: (0.5, 1, 2),
+            5120: (1, 2),
+            6144: (1, 2),
+            7168: (1, 2),
+            8192: (1, 2, 4),
+            9216: (2, 4),
+            10240: (2, 4),
+            11264: (2, 4),
+            12288: (2, 4),
+            13312: (2, 4),
+            14336: (2, 4),
+            15360: (2, 4),
+            16384: (2, 4),
+            17408: (4,),
+            18432: (4,),
+            19456: (4,),
+            20480: (4,),
+            21504: (4,),
+            22528: (4,),
+            23552: (4,),
+            24576: (4,),
+            25600: (4,),
+            26624: (4,),
+            27648: (4,),
+            28672: (4,),
+            29696: (4,),
+            30720: (4,),
+        }
+        if memory not in permitted_memory_vcpu:
+            raise JobBlueprintCreationError(f"memory {memory} not acceptable")
+        if vcpu not in permitted_memory_vcpu[memory]:
+            raise JobBlueprintCreationError(f"vcpu {vcpu} not acceptable")
+
+        containerProperties = {
+            "image": image,
+            # When this parameter is true, the container is given read-only access to its
+            # root file system. This parameter maps to ReadonlyRootfs in the Create a
+            # container section of the Docker Remote API and the --read-only option to
+            # docker run .
+            "readonlyRootFilesystem": True,
+            "resourceRequirements": [
+                {"type": "VCPU", "value": str(vcpu)},
+                {"type": "MEMORY", "value": str(memory)},
+            ],
+        }
+        # optionally add a command if specified
+        if command:
+            containerProperties["command"] = command
+
+        # send the actual client request
+        response = batch_client.register_job_definition(
+            jobDefinitionName=name,
+            type="container",
+            containerProperties=containerProperties,
+            platformCapabilities=["FARGATE"],
+        )
+
+        return str(response["jobDefinitionArn"])  # , int(response["revision"])
+
+    def _get_or_create_blueprint(
+        self,
+        batch_client,
+        name: str,
+        image: str,
+        vcpu: Union[float, int],
+        memory: int,
+        command: Iterable[str],
+    ) -> str:
+
+        # Note: this is vulnerable to race conditions if another process creates between
+        # the get and the create.
+        existing = self._get_blueprint(batch_client, name, image, vcpu, memory, command)
+        if existing:
+            return existing
+        else:
+            return self._create_blueprint(
+                batch_client, name, image, vcpu, memory, command
+            )
+
     def build(
         self,
         service_role_arn: str,
@@ -244,6 +412,10 @@ class Builder:
         subnet_ids: Iterable[str],
         image_name: str,
         image_tag: str,
+        image_repo: Union[str, None],
+        vcpu: Union[float, int] = 0.25,
+        memory: int = 512,
+        command: Iterable[str] = [],
         name_prefix: str = "chorecoral",
     ):
 
@@ -251,11 +423,20 @@ class Builder:
         # see https://docs.aws.amazon.com/batch/latest/userguide/service_IAM_role.html
         # We should detect and use this role if no service role is given.
 
-        # create a name for this in general
-        name = f"{name_prefix}_{image_name}_{image_tag}"
+        # TODO when a defauult execution rols is created its called ecsTaskExecutionRole
+        # see https://docs.aws.amazon.com/batch/latest/userguide/execution-IAM-role.html
+        # We should detect and use this role if no execution role is given
 
+        # build a single string for the image to be used
+        image_full = image_name
+        if image_tag:
+            image_full = image_full + ":" + image_tag
+        if image_repo:
+            image_full = image_repo + "/" + image_full
+
+        # create a name for this in general
         # name must only be alphanumeric, with _- in middle
-        name = re.sub("[^A-Za-z0-9_-]", "_", name)
+        name = re.sub("[^A-Za-z0-9_-]", "_", name_prefix + "_" + image_full)
         if not re.match("^[A-Za-z0-9][A-Za-z0-9_-]{1,126}[A-Za-z0-9]$", name):
             raise ValueError(f"name invalid '{name}'")
 
@@ -270,6 +451,9 @@ class Builder:
         # ensure queue exists
         self._get_or_create_queue(batch_client, name, compute_arn)
 
-        # TODO ensure blueprint exists
+        # ensure blueprint exists
+        self._get_or_create_blueprint(
+            batch_client, name, image_full, vcpu, memory, command
+        )
 
         # TODO create job
